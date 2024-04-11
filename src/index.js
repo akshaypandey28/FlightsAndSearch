5f5573a28867c579655917bbf313a44c56aebea8
Akshaynitian <akshaychaman2004@gmail.com>
2024-04-11 14:38:05 +0530

    set up body parser

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express=require("express");
-
+const bodyParser=require('body-parser');
 const {PORT}=require('./config/serverConfig.js')
 
 const setupAndStartServer= async () => {
     //create the express object
     const app=express();
+    app.use(bodyParser.json)
+    app.use(bodyParser.urlencoded)
     app.listen(PORT, () => {
         console.log(`Server started at ${PORT}`);
         //console.log(process.env);
